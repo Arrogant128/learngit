9e837e2f253a32c6ac4a4d5f08170c52086b312b
Arrogant128 <978614237@qq.com>
2019-02-28 22:45:19 +0800

    1:11

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,16 +27,22 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <el-radio v-model="radio" label="1"/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    },
+    data () {
+      return {
+        radio: '1'
+      }
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
